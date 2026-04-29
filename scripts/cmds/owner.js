47fa00679e1d5dadda3836c428f368bb21340440
Itzmaidul6 <itzmaidul6@gmail.com>
2026-04-29 23:52:00 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "owner",
     version: "1.3.0",
-    author: "Mᴏʜᴀᴍᴍᴀᴅ Aᴋᴀsʜ",
+    author: "—"𝐈𝐭'𝐳 𝐌𝐚𝐢𝐝𝐮𝐥🫰",
     role: 0,
     shortDescription: "Owner information with image",
     category: "Information",
@@ -18,17 +18,17 @@ module.exports = {
   onStart: async function ({ api, event }) {
     const ownerText = 
 `╭─ 👑 Oᴡɴᴇʀ Iɴғᴏ 👑 ─╮
-│ 👤 Nᴀᴍᴇ       : Mᴏʜᴀᴍᴍᴀᴅ Aᴋᴀsʜ
-│ 🧸 Nɪᴄᴋ       : Aᴋᴀsʜ
+│ 👤 Nᴀᴍᴇ       : Mᴏʜᴀᴍᴍᴀᴅ 𝐌𝐚𝐢𝐝𝐮𝐥
+│ 🧸 Nɪᴄᴋ       : —"𝐈𝐭'𝐳 🫶🫰
 │ 🎂 Aɢᴇ        : 18+
 │ 💘 Rᴇʟᴀᴛɪᴏɴ : Sɪɴɢʟᴇ
 │ 🎓 Pʀᴏғᴇssɪᴏɴ : Sᴛᴜᴅᴇɴᴛ
 │ 📚 Eᴅᴜᴄᴀᴛɪᴏɴ : Iɴᴛᴇʀ 2ɴᴅ Yᴇᴀʀ
-│ 🏡 Lᴏᴄᴀᴛɪᴏɴ : 𝐃𝐡𝐚𝐤𝐚 - 𝐆𝐚𝐳𝐢𝐩𝐮𝐫
+│ 🏡 Lᴏᴄᴀᴛɪᴏɴ : 𝗥𝗮𝗻𝗴𝗽𝘂𝗿 - 𝗞𝘂𝗿𝗶𝗴𝗿𝗮𝗺
 ├─ 🔗 Cᴏɴᴛᴀᴄᴛ ─╮
-│ 📘 Facebook  : fb.com/akashx404 
-│ 💬 Messenger: m.me/akashx404 
-│ 📞 WhatsApp  : wa.me/01933165880
+│ 📘 Facebook  : Mohammad.Maidul.20 
+│ 💬 Messenger: m.Mohammad.Maidul.20
+│ 📞 WhatsApp  : wa.me/01707791855
 ╰────────────────╯`;
 
     const cacheDir = path.join(__dirname, "cache");
