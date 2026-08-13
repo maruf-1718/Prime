1291fc8d653b87232e6025c64dc23e24b08bd135
Mohammad Maruf <yourmarufb@gmail.com>
2026-08-14 01:09:21 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports = {
   config: {
     name: "owner",
-    version: "1.3.0",
+    version: "1.0.0",
     author: "Mohammad Maruf",
     role: 0,
     shortDescription: "Owner information with image",
@@ -17,19 +17,27 @@ module.exports = {
 
   onStart: async function ({ api, event }) {
     const ownerText = 
-`╭─ 👑 Oᴡɴᴇʀ Iɴғᴏ 👑 ─╮
-│ 👤 Nᴀᴍᴇ       : Mohammad Maruf
-│ 🧸 Nɪᴄᴋ       : Maruf
-│ 🎂 Aɢᴇ        : 16+
-│ 💘 Rᴇʟᴀᴛɪᴏɴ : Sɪɴɢʟᴇ
-│ 🎓 Pʀᴏғᴇssɪᴏɴ : Sᴛᴜᴅᴇɴᴛ
-│ 📚 Eᴅᴜᴄᴀᴛɪᴏɴ : Secret
-│ 🏡 Lᴏᴄᴀᴛɪᴏɴ : Rangpur - Kurigram
-├─ 🔗 Cᴏɴᴛᴀᴄᴛ ─╮
-│ 📘 Facebook  : Mohammad Maruf
-│ 💬 Telegram : ❤️💚💛🩷🩵💙 
-│ 📞 WhatsApp  : ❤️💚💛🩷🩵💙
-╰────────────────╯`;
+`╭──── 👑 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨 👑 ─────╮
+│ ━━━━━━━━ 𝐍𝐚𝐦𝐞 ━━━━━━━━
+│         𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐌𝐚𝐫𝐮𝐟 🤷🏻‍♂️
+│ ━━━━━━━━ 𝐍𝐢𝐜𝐤 ━━━━━━━━━
+│                 𝐈𝐬𝐡𝐫𝐚𝐭 🎀 
+│ ━━━━━━━━ 𝐀𝐠𝐞 ━━━━━━━━━━
+│                 𝟏𝟔+ 👶🏻
+│ ━━━━━━━ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧 ━━━━━━━━
+│                𝐒𝐢𝐧𝐠𝐥𝐞 🥹
+│ ━━━━━━━━ 𝐖𝐨𝐫𝐤 ━━━━━━━━━
+│                 𝐒𝐭𝐮𝐝𝐞𝐧𝐭 😎
+│ ━━━━━━━ 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 ━━━━━━━
+│                  𝐒𝐞𝐜𝐫𝐞𝐭 🤫
+│ ━━━━━━━━ 𝐋𝐨𝐜𝐚𝐭𝐢𝐨𝐧 ━━━━━━━
+│            𝐊𝐮𝐫𝐢𝐠𝐫𝐦,𝐊𝐚𝐜𝐚𝐤𝐚𝐭𝐚 📍
+├─────────────────────╯
+├───── 🔗 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 ────────╮
+│ 📘 𝐅𝐁  : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐌𝐚𝐫𝐮𝐟
+│ 💬 𝐓𝐆  : @𝐦𝐚𝐫𝐮𝐟_𝟏𝟕𝟏𝟖
+│ 📞 𝐖𝐀  : @𝐦𝐚𝐫𝐮𝐟_𝟏𝟕𝟏𝟖
+╰─────────────────────╯`;
 
     const cacheDir = path.join(__dirname, "cache");
     const imgPath = path.join(cacheDir, "owner.jpg");
